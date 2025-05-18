defcbbb44eb952b2854850fa39a2d346506067b3
herenti <106109941+herenti@users.noreply.github.com>
2025-05-18 13:53:04 -0700

    Update anpanlib.rs

--- anpanlib.rs
+++ anpanlib.rs
@@ -4,28 +4,23 @@ fn g_server(mut group: String) -> i64{
 
     let weights = [[5, 75],[6, 75],[7, 75],[8, 75],[16, 75],[17, 75],[18, 75],[9, 95],[11, 95],[12, 95],[13, 95],[14, 95],[15, 95],[19, 110],[23, 110],[24, 110],[25, 110],[26, 110],[28, 104],[29, 104],[30, 104],[31, 104],[32, 104],[33, 104],[35, 101],[36, 101],[37, 101],[38, 101],[39, 101],[40, 101],[41, 101],[42, 101],[43, 101],[44, 101],[45, 101],[46, 101],[47, 101],[48, 101],[49, 101],[50, 101],[52, 110],[53, 110],[55, 110],[57, 110],[58, 110],[59, 110],[60, 110],[61, 110],[62, 110],[63, 110],[64, 110],[65, 110],[66, 110],[68, 95],[71, 116],[72, 116],[73, 116],[74, 116],[75, 116],[76, 116],[77, 116],[78, 116],[79, 116],[80, 116],[81, 116],[82, 116],[83, 116],[84, 116]];
 
-    group = group.replace("-","q");
-    group = group.replace("_","q");
-
+    group = group.replace("-","q").replace("_","q");
     let a = if group.len() > 6 {
-        let a_len = std::cmp::min(3, group.len() - 5);
-        let a_substr = &group[6..6+a_len];
-        let a_int = i64::from_str_radix(a_substr, 36).unwrap();
-        let a = a_int as f64;
-        if a <= 1000.0 {
-            1000.0
-        }
-        else {
+        let a = if group.len() >= 9 {
+        &group[6..9]
+        } else {
+            &group[6..]
+        };
+        let a = i64::from_str_radix(a, 36).unwrap() as f64;
+        let a = f64::max(1000.0, a);
         a
-        }
     }
     else{
         1000.0
     };
-    let b_len = std::cmp::min(5, group.len());
-    let b_substr = &group[..b_len];
-    let b_int = i64::from_str_radix(b_substr, 36).unwrap();
-    let b = b_int as f64;
+    let b = std::cmp::min(5, group.len());
+    let b = &group[..b];
+    let b = i64::from_str_radix(b, 36).unwrap() as f64;
     let num = (b / a) % 1.0;
     let mut anpan = 0.0;
     let mut s_number = 0;
